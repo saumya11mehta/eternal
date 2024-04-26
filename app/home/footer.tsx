41a0beb0c5927ed35952808aefe03e753c6028a5
saumya11mehta <115432888+saumya11mehta@users.noreply.github.com>
2024-04-26 12:19:21 -0400

    Made date dynamic

--- app/home/footer.tsx
+++ app/home/footer.tsx
@@ -1,10 +1,11 @@
 export default function HomeFooter() { 
+    let year = new Date().getFullYear();
     return (
         <footer className="fixed bottom-0 left-0 z-20 w-full p-4 shadow md:flex md:items-center md:justify-between md:p-6">
             <div className="flex items-center justify-between w-full">
             <div className="flex-grow basis-full border border-0 border-t-2 border-t-gray-900">
                 <div className="p-5 text-gray-500">
-                    &copy; Eternal 2023
+                    &copy; Eternal { year.toString() }
                 </div>
             </div>
             </div>
